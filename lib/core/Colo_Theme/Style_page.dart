bcffa84c291b14e9a8c8599a060beb34dbe58c68
ahmed hashem <ahmedhashem20021012@gmail.com>
2024-10-02 20:04:18 +0300

    updates

---
+++ lib/core/Colo_Theme/Style_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:task_app/core/Colo_Theme/Color_page.dart';
+
+abstract class Styles {
+  static TextStyle textStyle24 = const TextStyle(
+    color: Colors.black,
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle textStyle10 = const TextStyle(
+    color: ColorApp.primaryColor,
+    fontSize: 15,
+    fontWeight: FontWeight.w600,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/Features/splash_screans/Splash_page.dart';
+
+void main() {
+  runApp(const ChatApp());
+}
 
 class ChatApp extends StatelessWidget {
   const ChatApp({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Chat App"),),
-        body: Text("Chat App"),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
+      home: const HomePage(),
     );
   }
 }
